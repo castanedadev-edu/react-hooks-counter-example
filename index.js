ccd316259e93463280a810f074f0b3731366c8a3
castanedadev-edu <70346724+castanedadev-edu@users.noreply.github.com>
2022-09-23 01:40:34 -0600

    Replace functional component by class component

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
-import React, { useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 
-function Counter() {
-  const [counter, setCounter] = useState(0);
-
-  function increment() {
-    setCounter(counter+1);
+class Counter extends React.Component {
+  state = {
+    counter: 0,
+  };
+  increment = () => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+  componentDidUpdate() {
+    alert('Number of clicks: ' + this.state.counter);
   }
-  return <div>
-  <p>{counter}</p>
-  <button onClick={increment}>Increment</button>
-  </div>;
+  render() {
+    return (
+      <div>
+        <p>{this.state.counter}</p>
+        <button onClick={this.increment}>Increment</button>
+      </div>
+    );
+  }
 }
 
-const el = <Counter />; 
-ReactDOM.render(
-  el, 
-  document.getElementById('root')
-);
+const el = <Counter />;
+ReactDOM.render(el, document.getElementById('root'));
